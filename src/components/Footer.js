e55d3d1ce5527b1bae563a521579478d475c18c4
Tim <tfdeleon@buffalo.edu>
2023-08-10 16:15:07 -0400

    added styling

---
+++ src/components/Footer.js
@@ -0,0 +1,38 @@
+import { Container, Row, Col } from "react-bootstrap";
+import github from "../assets/img/github.svg";
+import linkedIn from "../assets/img/linkedin.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-item-center footer-bx">
+          <Col xs={6} sm={6}>
+            <div className="social-icon">
+              <a
+                href="https://www.linkedin.com/in/timothy-de-leon197/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={linkedIn} alt=""/>
+              </a>
+              <a
+                href="https://github.com/tfdeleon"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={github} alt="" />
+              </a>
+            </div>
+            <p>Thanks for stopping by!</p>
+          </Col>
+          <Col xs={6} sm={6} className="align-self-end">
+            <a href="#home">
+              <h1>Hope to hear from You!</h1>
+            </a>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
---
+++ src/components/Projects.js
@@ -0,0 +1,47 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import overrated from "../assets/img/overrated.gif";
+import commonCrave from "../assets//img/commonCrave.gif"
+import { Footer } from "./Footer";
+
+export const Projects = () => {
+  const projects = [
+    {
+      title: "Common Crave",
+      description:
+        "A restaurant recommendation website where friends can create groups to find a restaurant sharing their common craving while accommodating everyone's location.",
+      imgUrl: commonCrave,
+      gitUrl: "https://commoncrave.com/",
+      technologies: "Node.js, Express.js, MongoDB, React, Redux, Tailwind, AWS, Nginx, Ubuntu"
+    },
+    {
+      title: "Overrated",
+      description:
+      "A dating web application where users can find their soulmate by weeding out bad seeds with the implementation of dating reviews",
+      imgUrl: overrated,
+      gitUrl: "https://github.com/yeseniaramirez14/overrated",
+      technologies: "FastAPI, PostgreSQL, React, Bootstrap"
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col>
+            <div className="project-bx">
+              <hr className="divider"></hr>
+              <h2>Projects</h2>
+            </div>
+          </Col>
+        </Row>
+        <Row xs={1} sm={1} md={1} lg={2} xl={2} xxl={2}>
+          {projects.map((project, index) => {
+            return <ProjectCard key={index} {...project} />;
+          })}
+        </Row>
+      </Container>
+      <Footer />
+    </section>
+  );
+};
